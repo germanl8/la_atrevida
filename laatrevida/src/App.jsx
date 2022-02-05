8fa45e429cb8b39373536a3adbfe80497aa54600
German Leguizamon <96202044+germanl8@users.noreply.github.com>
2022-02-05 09:13:37 -0300

    primer commit

---
+++ laatrevida/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Barrasup from './components/Barrasup';
+import './App.css';
+
+const App = () => {
+
+    return (
+        <div id='container'>
+            <Barrasup></Barrasup>
+        </div>
+
+    )
+
+};
+export default App;
---
+++ laatrevida/src/components/Barrasup.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Barrasup = () => {
+
+    return (
+        <div id="container-bar">
+            {/*<h1>La Atrevida</h1>*/}
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Rooms & Rates</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Our Property</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Location</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+};
+export default Barrasup;
